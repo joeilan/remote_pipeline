96a2e4e13ef1ddab4ac5a369041036651d384a69
Ilangathir Joseph <joeilan@hotmail.com>
2022-11-09 07:50:25 +0530

    2

--- devops-pipeline/src/main/java/devops/pipeline/devops_pipeline/App.java
+++ devops-pipeline/src/main/java/devops/pipeline/devops_pipeline/App.java
@@ -1,13 +1,29 @@
 package devops.pipeline.devops_pipeline;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class App {
+	public static WebDriver driver;
+
+	public static void main(String[]  args) throws MalformedURLException, InterruptedException{
+
+ 		String URL = "https://www.DemoQA.com";
+ 		String Node = "https://192.168.62.202:4444/wd/hub";
+ 		DesiredCapabilities capabilities = new DesiredCapabilities();
+ 		
+ 		
+		capabilities.setBrowserName("chrome");
+ 				
+
+ 		driver = new RemoteWebDriver(new URL(Node), capabilities);
+
+ 		driver.navigate().to(URL);
+ 		Thread.sleep(5000);
+ 		driver.quit();
+ 	}		
 }
